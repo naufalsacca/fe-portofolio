8da5de31ccf6877c8a9ed7dddf4468d6840d0451
naufalsacca <naufalsacca2006@gmail.com>
2025-12-28 23:12:59 +0700

    Add files via upload

---
+++ portofolio.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById("themeToggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggleBtn.textContent =
+    document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
+
+
